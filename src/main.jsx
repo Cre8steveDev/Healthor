8994c4006a46b1cca0d537fd8ebd1c5286346a8f
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-30 13:22:54 +0100

    The real house cleaning

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -5,9 +6,7 @@ import "./index.css";
 import StateProvider from "./components/StateProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <StateProvider>
-      <App />
-    </StateProvider>
-  </React.StrictMode>
+  <StateProvider>
+    <App />
+  </StateProvider>
 );
